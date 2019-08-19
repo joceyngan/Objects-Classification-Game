62897c96e1c46092988bb459df173fa6a93ef4d0
pongleung <koonpong@gmail.com>
2019-08-19 14:10:57 +0800

    Add Menu Button to tutor for Delete Expire Room OR delete All Room

--- app/src/main/java/com/schoolofai/objectclassificationgame/tutor/tutorWelcome.java
+++ app/src/main/java/com/schoolofai/objectclassificationgame/tutor/tutorWelcome.java
@@ -4,30 +4,44 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Switch;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firebase.firestore.SetOptions;
+import com.google.firebase.firestore.Transaction;
+import com.google.firebase.firestore.WriteBatch;
+import com.google.firebase.firestore.model.Document;
 import com.schoolofai.objectclassificationgame.R;
 import com.schoolofai.objectclassificationgame.models.Room;
 
+import java.util.Date;
 import java.util.Random;
 
 import static com.schoolofai.objectclassificationgame.tutor.ReadyRoomFragment.listenerChange;
 
-public class tutorWelcome extends AppCompatActivity implements View.OnClickListener {
+public class tutorWelcome extends AppCompatActivity implements View.OnClickListener, Toolbar.OnMenuItemClickListener {
 
     protected static String roomNum;
     protected static Room room;
 
     protected static Button buttonCreate, buttonStart, buttonLeaveRoom;
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,6 +66,8 @@ public class tutorWelcome extends AppCompatActivity implements View.OnClickListe
         buttonCreate = findViewById(R.id.buttonCreate);
         buttonStart = findViewById(R.id.buttonStart);
         buttonLeaveRoom = findViewById(R.id.buttonLeaveRoom);
+        toolbar = findViewById(R.id.toolbar);
+        toolbar.inflateMenu(R.menu.menu_tutor);
 
     }
 
@@ -61,6 +77,7 @@ public class tutorWelcome extends AppCompatActivity implements View.OnClickListe
         buttonStart.setOnClickListener(this);
         buttonStart.setClickable(false);
         buttonLeaveRoom.setOnClickListener(this);
+        toolbar.setOnMenuItemClickListener(this);
     }
 
     @Override
@@ -116,9 +133,55 @@ public class tutorWelcome extends AppCompatActivity implements View.OnClickListe
         super.onDestroy();
         db.collection("rooms").document(roomNum).delete();
         Log.e("Welcome onDestroy", "onDestroy");
-        if (listenerChange !=null){
+        if (listenerChange != null) {
             listenerChange.remove();
         }
         room = null;
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.deleteRoom:
+                Date date = new Date();
+                System.out.println(date.getTime());
+                System.out.println(date);
+                date.setTime(date.getTime() - 1800000);
+                System.out.println(date.getTime());
+                System.out.println(date);
+                Query documentSnapshot = db.collection("rooms").whereLessThan("timestamp", date);
+                documentSnapshot.get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                        WriteBatch batch = db.batch();
+                        if (queryDocumentSnapshots.size() > 0) {
+                            for (DocumentSnapshot doc : queryDocumentSnapshots.getDocuments()) {
+                                if (!roomNum.equals(doc.getId())) {
+                                    batch.delete(db.collection("rooms").document(doc.getId()));
+                                }
+                            }
+                        }
+                        batch.commit();
+                    }
+                });
+                break;
+            case R.id.deleteAllRoom:
+                db.collection("rooms").get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                        WriteBatch batch = db.batch();
+                        if (queryDocumentSnapshots.size() > 0) {
+                            for (DocumentSnapshot doc : queryDocumentSnapshots.getDocuments()) {
+                                if (!roomNum.equals(doc.getId())) {
+                                    batch.delete(db.collection("rooms").document(doc.getId()));
+                                }
+                            }
+                        }
+                        batch.commit();
+                    }
+                });
+                break;
+        }
+        return false;
     }
 }
