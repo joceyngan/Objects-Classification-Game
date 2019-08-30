473bdc9710a852c9cccd4f3a16225146b231d6b8
pongleung <koonpong@gmail.com>
2019-08-30 13:02:48 +0800

    final model

--- app/src/main/java/com/schoolofai/objectclassificationgame/tflite/ClassifierFloatMobileNet.java
+++ app/src/main/java/com/schoolofai/objectclassificationgame/tflite/ClassifierFloatMobileNet.java
@@ -57,7 +57,7 @@ public class ClassifierFloatMobileNet extends Classifier {
     // you can download this file from
     // see build.gradle for where to obtain this file. It should be auto
     // downloaded into assets.
-    return "model20.tflite";
+    return "model33.tflite";
   }
 
   @Override
